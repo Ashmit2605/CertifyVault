54f937fabd850e21b8083d844c43fda0422fc3ba
Siddhi-99 <siddhi.patil2512@gmail.com>
2026-08-18 00:43:51 +0530

    Resolve merge conflicts and update Holder Dashboard

--- Website/vite.config.ts
+++ Website/vite.config.ts
@@ -1,19 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
 });
-
-
-=======
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-})
->>>>>>> d9232f0c95872dc01e0ffa7aef3807efefa03fc5
